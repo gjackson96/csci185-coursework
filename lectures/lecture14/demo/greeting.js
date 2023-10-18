ede1a4e46a0b54c42d76e80733b17dfe55e394dd
GabeJackson <gjackso4@unca.edu>
2023-10-18 16:16:31 -0400

    upadtes to hm5

--- lectures/lecture14/demo/greeting.js
+++ lectures/lecture14/demo/greeting.js
@@ -1,7 +1,13 @@
 function sayHello() {
     // your code here...
+    document.querySelector("#message").innerHTML ='Oh hey.';
+    document.querySelector('#message').style.color = "red";
 }
 
 function sayGoodbye() {
     // your code here...
+    document.querySelector("#message").innerHTML ='Buh bye.';
+    document.querySelector('#message').style.color = "blue";
+
+
 }
