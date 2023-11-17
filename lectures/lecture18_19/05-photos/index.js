2396662fd811c1eb8cb936c2b06bab5b9116c8fe
GabeJackson <gjackso4@unca.edu>
2023-11-17 00:46:15 -0500

    added final prject part b

--- lectures/lecture18_19/05-photos/index.js
+++ lectures/lecture18_19/05-photos/index.js
@@ -17,9 +17,19 @@ const photos = [
     'images/daisy1.jpg'
 ];
 
+// what do you want to repeat:
+// 1. create a div tag with a background image
+// 2. target the item with the class of cards
+// 3. how long? until we are at the last photo
+// 4. what changes each time? the slot or index where the photo is stored
+
 // Create a card for every image in the photos list using a for...of loop and
 // template variables
-let template = `
-    <div class="card" style="background-image:url('images/poppies.jpg')"></div>
-`;
-document.querySelector('.cards').insertAdjacentHTML('beforeend', template);
+let counter = 0;
+while (counter < 16) {
+    let template = `
+        <div class="card" style="background-image:url('${ photos[counter]}')"></div>
+    `;
+    counter += 1;
+    document.querySelector('.cards').insertAdjacentHTML('beforeend', template);
+}
