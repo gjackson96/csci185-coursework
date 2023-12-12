6272a33557156cb5fb55a39ef0ce8a3205dde84b
GabeJackson <gjackso4@unca.edu>
2023-12-11 19:42:50 -0500

    updated project

--- project/index.js
+++ project/index.js
@@ -1,1 +1,9 @@
-// 
+
+function openLightbox(imageSrc) {
+    document.getElementById('lightbox-image').src = imageSrc;
+    document.getElementById('character-lightbox').style.display = 'flex';
+}
+
+function closeLightbox() {
+    document.getElementById('character-lightbox').style.display = 'none';
+}
